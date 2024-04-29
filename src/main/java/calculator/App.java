8a0105127063c9a9e754b8026f985996e68aabd7
Robbie <robbie331@>
2024-04-29 14:40:55 +0900

    Level 1 -3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
+          int result = 0;
 
           System.out.print("첫 번째 숫자를 입력하세요: ");
           int num1 = sc.nextInt();
@@ -13,6 +14,28 @@ public class App {
           System.out.print("사칙연산 기호를 입력하세요: ");
           Character sa = sc.next().charAt(0);
 
+        switch (sa) {
+            case'+' :  {
+                result = num1 + num2;
+                break;
+            }
+            case'-' :  {
+                result = num1 - num2;
+                break;
+            }
+            case'*' :  {
+                result = num1 * num2;
+                break;
+            }
+            case'/' :  {
+                if(num2 != 0) {
+                    result = num1 / num2;
+                    break;
+                }
+            }
+
+        }
+          System.out.println("결과: " + result);
      }
 
 }
