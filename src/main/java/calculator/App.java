2985fb2665932f8e4b9a5dd1f733c7555ce5be8f
yj <belle521834@gmail.com>
2024-04-29 14:58:23 +0900

    test

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,16 +3,16 @@ package calculator;
 import java.util.Scanner;
 
 public class App {
-     public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
-          int result = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
 
-          System.out.print("첫 번째 숫자를 입력하세요: ");
-          int num1 = sc.nextInt();
-          System.out.print("두 번째 숫자를 입력하세요:");
-          int num2 = sc.nextInt();
-          System.out.print("사칙연산 기호를 입력하세요: ");
-          Character sa = sc.next().charAt(0);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요:");
+        int num2 = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        Character sa = sc.next().charAt(0);
 
         switch (sa) {
             case'+' :  {
@@ -35,8 +35,8 @@ public class App {
             }
 
         }
-          System.out.println("결과: " + result);
-     }
+        System.out.println("결과:" + result);
+    }
 
 }
 
