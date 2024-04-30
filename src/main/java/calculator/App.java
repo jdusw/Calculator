d516621cc01d48e26c4d59d688bb189110e651b3
yj <belle521834@gmail.com>
2024-05-01 01:21:29 +0900

    lever 1 -8

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -48,12 +48,20 @@ public class App {
             System.out.println("결과:" + result);
 
             arr.add(result);
-            System.out.println(arr.toString());
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String remove = sc.next();
             if(remove.equals("remove")) {
                 arr.remove(0);
+            }
+
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            String inquiry = sc.next();
+            if(inquiry.equals("inquiry")) {
+                for(int a : arr ){
+                    System.out.print(a + " ");
+                }
+                System.out.println();
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
